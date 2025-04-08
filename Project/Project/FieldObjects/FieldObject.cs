0be9c2e308fc3d0ced517a24cb62b9fbaffc8f0c
01stone01 <hagwhr2@gmail.com>
2025-04-08 17:37:20 +0900

    [feat] adjust player spawn position

--- Project/Project/FieldObjects/FieldObject.cs
+++ Project/Project/FieldObjects/FieldObject.cs
@@ -14,7 +14,22 @@ public class FieldObject : GameObject
         Util.FieldTriangle("밖","field");
         if (Player.Instance.NextPlace != Player.Instance.CurrentPlace)
         {
-            Player.Instance.SetPosition(new Vector2(5,3));
+            if(Player.Instance.CurrentPlace.Name == "smithy")
+            {
+                Player.Instance.SetPosition(new Vector2(7, 5));
+            }
+            else if(Player.Instance.CurrentPlace.Name == "casino")
+            {
+                Player.Instance.SetPosition(new Vector2(3, 2));
+            }
+            else if (Player.Instance.CurrentPlace.Name == "home")
+            {
+                Player.Instance.SetPosition(new Vector2(2, 3));
+            }
+            else
+            {
+                Player.Instance.SetPosition(new Vector2(11, 2));
+            }
         }
         Player.Instance.CurrentPlace = Player.Instance.NextPlace;
     }
